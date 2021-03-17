fe0d6aaa61b676ee62f49b51a49db3bb3f2089d1
Angela Coelho <angela.01.coelho@gmail.com>
2021-03-17 19:11:16 +0000

    Add tp05

---
+++ TP05/cmake-build-debug/lib/googletest-master/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/angela/Desktop/MIEIC/2y-2s/cal/tp05/lib/googletest-master/googlemock
+# Build directory: /home/angela/Desktop/MIEIC/2y-2s/cal/tp05/cmake-build-debug/lib/googletest-master/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
---
+++ TP05/cmake-build-debug/lib/googletest-master/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/angela/Desktop/MIEIC/2y-2s/cal/tp05/lib/googletest-master/googletest
+# Build directory: /home/angela/Desktop/MIEIC/2y-2s/cal/tp05/cmake-build-debug/lib/googletest-master/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
