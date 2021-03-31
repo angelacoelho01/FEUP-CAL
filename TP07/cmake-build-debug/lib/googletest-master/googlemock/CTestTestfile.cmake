3fbd76c994907ba1acea030647dc471fad3d7df1
Angela Coelho <angela.01.coelho@gmail.com>
2021-03-31 23:02:51 +0100

    Add tp07

---
+++ TP07/cmake-build-debug/lib/googletest-master/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/angel/Desktop/MIEIC/2nd_year/2nd_semester/CAL/FEUP-CAL/TP07/lib/googletest-master/googlemock
+# Build directory: C:/Users/angel/Desktop/MIEIC/2nd_year/2nd_semester/CAL/FEUP-CAL/TP07/cmake-build-debug/lib/googletest-master/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
---
+++ TP07/cmake-build-debug/lib/googletest-master/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/angel/Desktop/MIEIC/2nd_year/2nd_semester/CAL/FEUP-CAL/TP07/lib/googletest-master/googletest
+# Build directory: C:/Users/angel/Desktop/MIEIC/2nd_year/2nd_semester/CAL/FEUP-CAL/TP07/cmake-build-debug/lib/googletest-master/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
